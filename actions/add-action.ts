98b34343341c8094e7460f5692f492db8d518171
vitalii <0x1d46@gmail.com>
2024-07-06 21:35:55 +0200

    11

--- actions/add-action.ts
+++ actions/add-action.ts
@@ -6,25 +6,26 @@ import { put } from "@vercel/blob";
 import { Pinecone } from "@pinecone-database/pinecone";
 import OpenAI from "openai";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
-const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
-
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-  systemInstruction: `make a detailed description of the image with text`,
-});
-
-const session = model.startChat({
-  generationConfig: {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-  },
-});
 export const addAction = async (formData: FormData) => {
   try {
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+    const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
+
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction: `make a detailed description of the image with text`,
+    });
+
+    const session = model.startChat({
+      generationConfig: {
+        temperature: 1,
+        topP: 0.95,
+        topK: 64,
+        maxOutputTokens: 8192,
+      },
+    });
+
     const file = formData.get("file") as File;
     const description = formData.get("description") as string;
 
