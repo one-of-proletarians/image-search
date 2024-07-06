552ce5f2f6838194296b75bee48543ead104e55a
vitalii <0x1d46@gmail.com>
2024-07-06 22:05:44 +0200

    44

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
   if (!ids.length)
     return (
       <div className="w-full h-full flex items-center justify-center">
-        <h1 className="text-3xl font-bold">No results</h1>
+        <h1 className="text-3xl font-bold">No results {ids.toString()}</h1>
       </div>
     );
 
